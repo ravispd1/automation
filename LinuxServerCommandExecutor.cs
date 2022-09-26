b44707d9b717c3010fb07cdb145b2c234254461c
ravispd1 <114335102+ravispd1@users.noreply.github.com>
2022-09-25 18:02:46 -0700

    Create LinuxServerCommandExecutor.cs

---
+++ LinuxServerCommandExecutor.cs
@@ -0,0 +1,58 @@
+public class LinuxServerCommandExecutor : IServerCommandExecutor
+{
+	private readonly ITestRunner _runner;
+	private const string TerminalName = ""vt100;
+	private const int Columns = 255;
+	private const int Rows = 50;
+	private const int Width = 800;
+	private const int Height = 600;
+	private const int BufferSize = 1024;
+	
+	public LinuxServerCommandExecutor(ITestRunner runner)
+	{
+		_runner = runner;
+	}
+	
+	public string Execute(string host, string username, string password, string appId, string command)
+	{
+		var regex = CreateTerminalPromptRegex(host);
+		return Execute(host, username, password, appId, command, regex);
+	}
+	
+	public string Execute(string host, string username, string password, string appId, string command, string stopRegex)
+	{
+		var regex = CreateTerminalPromptRegex(stopRegex);
+		return Execute(host, username, password, appId, command, regex);
+	}
+	
+	public string Execute(string host, string username, string password, string appId, string command, Regex stopRegex)
+	{
+		LogInputParams(host, username, appId, command);
+		
+		using (var client = new SshClient(host, username, password))
+		{
+			try
+			{
+				client.Connect();
+				var sb = new StringBuilder();
+				var modes = new Dictionary<TerminalModes, unit>();
+				var promptRegex = CreateTerminalPromptRegex(host);
+				using (var stream = client.CreateShellStream(TerminalName, Columns, Rows, Width, Height, BufferSize, modes))
+				{
+					sb.Append(stream.Expect(promptRegex));
+					if(appId != null)
+					{
+						stream.Write($"dzdo -k su - {appId}\n");
+						stream.Expect($"[dzdo] password for {username}:");
+						stream.Write($"{password}\n");
+						sb.Append(stream.Expect(promptRegex));
+						
+					}
+					stream.Write($"{command}\n");
+					
+					var latestRead = string.Empty;
+				}
+			}
+		}
+	}
+}
